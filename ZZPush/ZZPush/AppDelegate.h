8b73a732ca2a7f80e94c34b46d621f4e89305cdf
Loren <hyl946@163.com>
2018-04-17 15:09:27 +0800

    推送Demo

---
+++ ZZPush/ZZPush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZZPush
+//
+//  Created by Loren on 2017/12/20.
+//  Copyright © 2017年 Loren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZZPush/ZZPush/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ZZPush
+//
+//  Created by Loren on 2017/12/20.
+//  Copyright © 2017年 Loren. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
